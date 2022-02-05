2e84b9a662a9b7eabd3b7d4d8fc354ba88a5e8c0
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-05 17:57:42 -0500

    commit tree 441a405784da2fab2544b3db8939922f5c3caf4b

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [dogImage, setDogImage] = useState('')
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(dog => {
+      setDogImage(dog)
+      setIsLoaded(true)
+    })
+  },[])
+
+  if (!isLoaded) return <p>Loading...</p>
+
+  return <img src={dogImage.message} alt="A Random Dog" />
+}
+
+export default App;
